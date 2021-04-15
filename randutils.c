0d67e00ee38f8d4fe06a5328bf5c5ded2dc78170
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2021-04-15 19:46:45 +0200

    :wrench: don't mistaken nxxm with the potential TEST_PROGRAM inside the library file.

--- randutils.c
+++ randutils.c
@@ -108,18 +108,3 @@ void random_get_bytes(void *buf, size_t nbytes)
 	return;
 }
 
-#ifdef TEST_PROGRAM
-int main(int argc __attribute__ ((__unused__)),
-         char *argv[] __attribute__ ((__unused__)))
-{
-	unsigned int v, i;
-
-	/* generate and print 10 random numbers */
-	for (i = 0; i < 10; i++) {
-		random_get_bytes(&v, sizeof(v));
-		printf("%d\n", v);
-	}
-
-	return EXIT_SUCCESS;
-}
-#endif /* TEST_PROGRAM */
